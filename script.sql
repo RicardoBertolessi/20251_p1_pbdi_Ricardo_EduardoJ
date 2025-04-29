c0e16e95f22e08b85b1f4d743d38fdb0174b216b
Ricardo Galdino <rgaldino510@gmail.com>
2025-04-29 11:31:40 -0300

    feat(p1): encontro aprovados com pais PhDs

--- script.sql
+++ script.sql
@@ -14,12 +14,30 @@ CREATE TABLE p1_pbdi_20251_(
 );
 
 COPY p1_pbdi_20251_(studentID, grade, preap_exam, father_edu, mother_edu, salary, preap_stud) 
-FROM 'C:\Users\aluno\Downloads\student_prediction(2).csv' DELIMITER ','
+FROM 'C:\Users\aluno\Documents\Ricardo_Galdino\P1\student_prediction (2).csv' DELIMITER ','
 CSV HEADER;
 
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
+
+DO $$
+DECLARE
+    cur_name_formacao_dos_pais REFCURSOR;
+    contador INT := 0;
+        SELECT * FROM p1_pbdi_20251_
+        WHERE mother_edu OR father_edu = '6'
+            AND grade != 0;
+BEGIN
+    OPEN cur_name_formacao_dos_pais;
+    LOOP
+        FETCH cur_name_formacao_dos_pais INTO cur_name_formacao_dos_pais;
+        contador : = contador + 1;
+    END LOOP;
+    CLOSE cur_name_formacao_dos_pais;
+    RAISE NOTICE 'numero de aprovados $', contador;
+END
+$$
 
 -- ----------------------------------------------------------------
 -- 3 Resultado em função dos estudos
